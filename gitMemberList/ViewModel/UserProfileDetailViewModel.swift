8cd8d247b317c3c58c30f982f3cddd7eb504f92f
Victor Huang <victor@mitake.com.tw>
2020-06-30 12:40:01 +0800

    add UserListViewModel and UserProfileDetailViewModel

---
+++ gitMemberList/ViewModel/UserProfileDetailViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  UserProfileDetailViewModel.swift
+//  gitMemberList
+//
+//  Created by APP技術部-黃一修 on 2020/6/30.
+//  Copyright © 2020 APP技術部-黃一修. All rights reserved.
+//
+
+import Foundation
+class UserProfileDetailViewModel {
+    
+}
