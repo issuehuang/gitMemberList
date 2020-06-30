3f389d53e1ad28c95dd2c25cbd97f53e3b42f041
Victor Huang <victor@mitake.com.tw>
2020-06-30 11:43:08 +0800

    Initial Commit

---
+++ gitMemberList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitMemberList
+//
+//  Created by APP技術部-黃一修 on 2020/6/30.
+//  Copyright © 2020 APP技術部-黃一修. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
