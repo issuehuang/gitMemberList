d1c7e921eaa17be1dc0f5585d4c2fce81c176f92
Victor Huang <victor@mitake.com.tw>
2020-06-30 12:33:43 +0800

    add UserProfileDetailViewController

---
+++ gitMemberList/ViewController/UserProfileDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserProfileDetailViewController.swift
+//  gitMemberList
+//
+//  Created by APP技術部-黃一修 on 2020/6/30.
+//  Copyright © 2020 APP技術部-黃一修. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
