91cc0249a655afb182c652b5197f3697377d58f7
Victor Huang <victor@mitake.com.tw>
2020-06-30 13:13:07 +0800

    add UserListTableViewCell and Xib

---
+++ gitMemberList/UserListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserListTableViewCell.swift
+//  gitMemberList
+//
+//  Created by APP技術部-黃一修 on 2020/6/30.
+//  Copyright © 2020 APP技術部-黃一修. All rights reserved.
+//
+
+import UIKit
+
+class UserListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
