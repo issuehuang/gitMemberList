636c06bdd64da521fd32b5fbf25cf700e078a180
Victor Huang <victor@mitake.com.tw>
2020-07-01 13:31:34 +0800

    setting UserListViewModel

--- gitMemberList/ViewModel/UserListViewModel.swift
+++ gitMemberList/ViewModel/UserListViewModel.swift
@@ -8,5 +8,55 @@
 
 import Foundation
 class UserListViewModel {
-
+    
+    var reloadTableViewClosure: (()->())?
+    var showAlertClosure: (()->())?
+    var updateLoadingStatus: (()->())?
+    var presenVCwithContent:((_ content:String)->())?
+    private var tableViewDatas:[UserProfileListModel] = [UserProfileListModel](){
+        didSet{
+            self.reloadTableViewClosure?()
+        }
+        
+    }
+    var isLoading: Bool = false {
+        didSet {
+            self.updateLoadingStatus?()
+        }
+    }
+    
+    var numberOfRows:Int{
+        return tableViewDatas.count
+    }
+    
+    var alertMessage: String? {
+        didSet {
+            self.showAlertClosure?()
+        }
+    }
+    var loginStr:String?{
+        didSet{
+            self.presenVCwithContent?(loginStr ?? "")
+        }
+    }
+    
+    func initFetch(){
+        self.isLoading  = true
+        ConnectionTools.share.getUserList { (success, error, userList) in
+            if let error = error {
+                self.alertMessage = "Oops! someting Wrong!\(error.localizedDescription)"
+            }else
+            {
+                self.isLoading = false
+                self.tableViewDatas = userList ?? [UserProfileListModel]()
+            }
+        }
+    }
+    func getUser(at indexPath:IndexPath)->UserProfileListModel{
+        return tableViewDatas[indexPath.row]
+    }
+    
+    func getUserLoginStr(at indexPath:IndexPath){
+        self.loginStr = tableViewDatas[indexPath.row].login
+    }
 }
