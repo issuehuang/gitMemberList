5ca1678ecf3f43a1639c69f657765befa7f25556
Victor Huang <victor@mitake.com.tw>
2020-07-01 13:39:01 +0800

    setting ViewControllers

--- gitMemberList/ViewController/UserListViewController.swift
+++ gitMemberList/ViewController/UserListViewController.swift
@@ -7,24 +7,75 @@
 //
 
 import UIKit
-
+import JGProgressHUD
 class UserListViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    lazy var viewModel: UserListViewModel = {
+        return UserListViewModel()
+    }()
+    let hud = JGProgressHUD(style: .dark)
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        initViewModel()
+        initView()
+    }
+    func initView(){
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.separatorStyle = .none
+        let nib = UINib(nibName: "UserListTableViewCell", bundle: nil)
+        self.tableView.register(nib, forCellReuseIdentifier: "UserListTableViewCell")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func initViewModel(){
+        
+        viewModel.presenVCwithContent = { [weak self] content in
+            let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "UserProfileDetailViewController") as! UserProfileDetailViewController
+            //vc.modalPresentationStyle = .popover
+            vc.loginStr = content
+            self?.present(vc, animated: true, completion: nil)
+            
+        }
+        viewModel.updateLoadingStatus = { [weak self] () in
+            DispatchQueue.main.async {
+                let isLoading = self?.viewModel.isLoading ?? false
+                if isLoading {
+                    self?.hud.show(in: self?.view ?? UIView())
+                }
+                else{
+                    self?.hud.dismiss()
+                }
+            }
+        }
+        viewModel.reloadTableViewClosure = { [weak self] () in
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+        }
+        viewModel.initFetch()
     }
-    */
-
+    
+    
+}
+extension UserListViewController:UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfRows
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "UserListTableViewCell", for: indexPath) as? UserListTableViewCell else{
+            fatalError()
+        }
+        let user =  viewModel.getUser(at: indexPath)
+        cell.userListCellModel = user
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        viewModel.getUserLoginStr(at: indexPath)
+    }
 }
--- gitMemberList/ViewController/UserProfileDetailViewController.swift
+++ gitMemberList/ViewController/UserProfileDetailViewController.swift
@@ -7,24 +7,68 @@
 //
 
 import UIKit
-
+import JGProgressHUD
 class UserProfileDetailViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    @IBOutlet weak var avatorImage: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var loginStrLabel: UILabel!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    
+    @IBOutlet weak var blogLabel: UILabel!
+    
+    lazy var viewModel: UserProfileDetailViewModel = {
+        return UserProfileDetailViewModel()
+    }()
+    var loginStr:String? = ""
+    let hud = JGProgressHUD(style: .dark)
 
-        // Do any additional setup after loading the view.
+    @IBAction func pressDismissBtn(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initViewModel()
+        initView()
     }
-    */
+    func initView(){
+        self.avatorImage.layer.cornerRadius = self.avatorImage.frame.size.width/2
+    }
+
+    func initViewModel(){
+        viewModel.reloadViewClosure = { [weak self]() in
+            DispatchQueue.main.async {
+                if let data = self?.viewModel.userDetail{
+                    self?.nameLabel.text = data.name
+                    self?.loginStrLabel.text = data.login
+                    self?.blogLabel.text = data.blog
+                    if  let url = URL(string: data.avatar_url ?? "") {
+                         let data = try! Data(contentsOf: url )
+                        self?.avatorImage.image = UIImage(data: data)
+                    }
+                }
+            }
+            
+            
+        }
+        viewModel.updateLoadingStatus = { [weak self] () in
+               DispatchQueue.main.async {
+                            let isLoading = self?.viewModel.isLoading ?? false
+                                      if isLoading {
+                                       self?.hud.show(in: self?.view ?? UIView())
+                   }
+                                      else{
+                                       self?.hud.dismiss()
+                   }
+                      }
+                  }
+        viewModel.initFetch(with: self.loginStr ?? "")
+    }
 
 }
