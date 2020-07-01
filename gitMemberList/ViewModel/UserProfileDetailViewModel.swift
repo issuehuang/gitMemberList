1e99323c518abd1c47d13690566b7d180d416540
Victor Huang <victor@mitake.com.tw>
2020-07-01 12:03:03 +0800

    setting UserProfileDetailViewModel

--- gitMemberList/ViewModel/UserProfileDetailViewModel.swift
+++ gitMemberList/ViewModel/UserProfileDetailViewModel.swift
@@ -8,5 +8,37 @@
 
 import Foundation
 class UserProfileDetailViewModel {
+    var reloadViewClosure: (()->())?
+    var showAlertClosure: (()->())?
+    var updateLoadingStatus: (()->())?
+    var userDetail:UserProfileDetailModel?{
+        didSet{
+            self.reloadViewClosure?()
+        }
+    }
     
+    
+    var isLoading: Bool = false {
+        didSet {
+            self.updateLoadingStatus?()
+        }
+    }
+    
+    var alertMessage: String? {
+        didSet {
+            self.showAlertClosure?()
+        }
+    }
+    func initFetch(with loginStr:String){
+        self.isLoading = true
+        ConnectionTools.share.getUserDetail(loginStr: loginStr) { (isSuccess, error, data) in
+            if let error = error {
+                self.alertMessage = "Oops! someting Wrong!\(error.localizedDescription)"
+            }else
+            {
+                 self.isLoading = false
+                self.userDetail = data
+            }
+        }
+    }
 }
